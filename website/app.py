10a535f414a53185ea59dec116d91e3f01e4ff83
Leonor Paiva GC <mleonorpaiva@gmail.com>
2022-09-05 16:04:35 +0100

    progress on SLT front office

--- website/app.py
+++ website/app.py
@@ -2,31 +2,28 @@ import streamlit as st
 import cv2
 from PIL import Image
 
-title_container = st.container()
-col1, col2 = st.columns([2, 5])
+#Display logo on the left and titleon the right
+col1, col2 = st.columns([1, 6])
 medical_image = Image.open('./medical symbol.png')
-with title_container:
-    with col1:
-        st.image(medical_image, width=100)
-    with col2:
-        st.markdown('<h1 style="float: left;">MEDIAL SERVICES</h1><img style="float: right;" src="MEDICAL SYMBOL.PNG" />', unsafe_allow_html=True)
+with col1:
+    st.image(medical_image, width=100)
+with col2:
+    #st.markdown('<h1 style="float: left;">MEDIAL SERVICES</h1><img style="float: right;" src="MEDICAL SYMBOL.PNG" />', unsafe_allow_html=True)
+    st.title("MEDICAL SERVICES")
 
-#medical_image = Image.open('./medical symbol.png')
-#st.image(medical_image, width=100, use_column_width=False)
+#Visualize patient info
+st.subheader("Patient details")
+st.text("Name: XXXXX XXXXX \nAge:XX\nGenre: XXXXX")
 
-st.title("Webcam Test")
-
-
-run = st.button("Translate patient's symptoms:")
-if run:
-    # print is visible in the server output, not in the page
-    print('button clicked!')
-
-
-frame_window = st.image([])
-cam = cv2.VideoCapture(0)
-
-while run:
-    ret, frame = cam.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_window.image(frame)
+#Open camera on the left and display symptoms on the right
+col1, col2 = st.columns([1, 1])
+with col1:
+    run = st.button("Translate patient's symptoms")
+    frame_window = st.image([])
+    cam = cv2.VideoCapture(0)
+    while run:
+        ret, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_window.image(frame)
+        #with col2:
+            #st.text("headache \ntired \nsore throat")
